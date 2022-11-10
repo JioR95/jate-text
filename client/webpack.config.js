57be0804ecb6d6a95bc86f0f669f8458ca1ce2e3
JioR95 <jiovani.rivera@rocketmail.com>
2022-11-09 20:04:28 -0500

    update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,14 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const { pathToFileURL } = require("url");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -21,7 +21,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E'
+        title: 'Webpack Plugin'
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -33,13 +33,19 @@ module.exports = () => {
         description: 'Takes notes with JavaScript!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
+          },
+          {
+            src: path.resolve("src/images/logo.png"),
+            size: "1024x1024",
+            destination: path.join("assets", "icons"),
+            purpose: "maskable",
           },
         ],
       }),
@@ -61,9 +67,8 @@ module.exports = () => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
+              presets: [["@babel/preset-env", {targets: "defaults"}]],
+            }
           },
         },
       ],
